ba2f43e0021154e7f261f2b5270d2c40633a1f81
Craole <iamcraole@gmail.com>
2023-05-16 23:15:31 -0500

    "Updating"

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use sqlx::Connection;
-use sqlx::Row;
 use std::error::Error;
-// use sqlx::postgres::PgPoolOptions;
+// use sqlx::Connection;
+use sqlx::Row;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().expect("Unable to load environment variables from .env file");
-    let db_url = std::env::var(DATABASE_URL_PRJ1).expect("Unable to read DATABASE_URL_PRJ1 env var");
+    let db_url =
+        std::env::var("DATABASE_URL_PRJ1").expect("Unable to read DATABASE_URL_PRJ1 env var");
+    let pool = sqlx::postgres::PgPool::connect(&db_url).await?;
 
-    let mut conn = sqlx::postgres::PgConnection::connect(&db_url).await?;
+    sqlx::migrate!("./migrations").run(&pool).await?;
 
-    let res = sqlx::query("SELECT 1 + 1 AS sum")
-        .fetch_one(&mut conn)
-        .await?;
+    let res = sqlx::query("SELECT 1 + 1 AS sum").fetch_one(&pool).await?;
 
     let sum: i32 = res.get("sum");
     println!("1 + 1 = {}", sum);
